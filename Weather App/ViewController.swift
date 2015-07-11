18d2185c14b283c7adc2f7a1c037eef749998226
Brandon <leebran@onid.oregonstate.edu>
2015-07-11 11:51:20 -0700

    Added API key [blank]

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentHumidityLabel: UILabel?
     @IBOutlet weak var currentPrecipitationLevel: UILabel?
     
+    private let forcastAPIKey = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
