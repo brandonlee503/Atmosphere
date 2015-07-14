7240b168d2ac7df12ac6eef52d3a97fff2a79de8
Brandon <leebran@onid.oregonstate.edu>
2015-07-14 02:35:57 -0700

    Updated CurrentWeather struct for data from network

--- Weather App/CurrentWeather.swift
+++ Weather App/CurrentWeather.swift
@@ -10,22 +10,28 @@ import Foundation
 
 struct CurrentWeather {
     
-    let temperature: Int
-    let humidity: Int
-    let precipProbability: Int
-    let summary: String
+    let temperature: Int?
+    let humidity: Int?
+    let precipProbability: Int?
+    let summary: String?
     
     // Initialize struct values towards plist values
     init(weatherDictionary: [String: AnyObject]) {
         
-        temperature = weatherDictionary["temperature"] as! Int
+        temperature = weatherDictionary["temperature"] as? Int
         
-        let humidityFloat = weatherDictionary["humidity"] as! Double
-        humidity = Int(humidityFloat * 100)
+        if let humidityFloat = weatherDictionary["humidity"] as? Double {
+            humidity = Int(humidityFloat * 100)
+        } else {
+            humidity = nil
+        }
+
+        if let precipProbabilityFloat = weatherDictionary["precipProbability"] as? Double {
+            precipProbability = Int(precipProbabilityFloat * 100)
+        } else {
+            precipProbability = nil
+        }
         
-        let precipProbabilityFloat = weatherDictionary["precipProbability"] as! Double
-        precipProbability = Int(precipProbabilityFloat * 100)
-        
-        summary = weatherDictionary["summary"] as! String
+        summary = weatherDictionary["summary"] as? String
     }
 }
