3df6452661abffd667270cc9042222f690fd395c
Brandon <leebran@onid.oregonstate.edu>
2015-07-22 00:09:29 -0700

    Removed old style

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -30,32 +30,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         retrieveWeatherForcast()
-        
-        
-        /* - OLD NOOB IMPLEMENTATION
-        // Declare base URL and call it in forcast URL for specific locations
-        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(forcastAPIKey)/")
-        let forcastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
-        
-        // Data object to fetch weather data (synchronous way)
-        /*
-        let weatherData = NSData(contentsOfURL: forcastURL!, options: nil, error: nil)
-        println(weatherData)
-        */
-        
-        // Use NSURLSession API to fetch data (asynchronous way)
-        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let session = NSURLSession(configuration: configuration)
-        
-        // Create NSURLRequest object
-        let request = NSURLRequest(URL: forcastURL!)
-        
-        let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data:
-            NSData!, response: NSURLResponse!, error: NSError!) -> Void in
-        })
 
-        dataTask.resume()
-        */
     }
 
     override func didReceiveMemoryWarning() {
