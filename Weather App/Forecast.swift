fce4f7dcce2247bccd0dd5dfcbf60f1882f9d67d
Brandon <leebran@onid.oregonstate.edu>
2015-08-17 00:12:02 -0700

    Added Forecast struct as wrapper for CurrentWeather/DailyWeather models

---
+++ Weather App/Forecast.swift
@@ -0,0 +1,24 @@
+//
+//  Forecast.swift
+//  Weather App
+//
+//  Created by Brandon Lee on 8/17/15.
+//  Copyright (c) 2015 Brandon Lee. All rights reserved.
+//
+
+import Foundation
+
+// Main purpose is to act as a wrapper for both the CurrentWeather and DailyWeather models
+struct Forecast {
+    var currentWeather: CurrentWeather?
+    var weekly: [DailyWeather] = []
+    
+    init(weatherDictionary: [String: AnyObject]?) {
+        // Parse the contents of dictionary and create populated instance of current weather
+        // Optional binding to make sure jsonDictionary returns non-nil value,
+        // if so cast it to a dictionary type and assign to variable
+        if let currentWeatherDictionary = weatherDictionary?["currently"] as? [String: AnyObject] {
+            currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        }
+    }
+}
