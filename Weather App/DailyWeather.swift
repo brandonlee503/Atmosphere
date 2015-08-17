be46b69ad3982efd4ed6c733b635fb2a26a48cb5
Brandon <leebran@onid.oregonstate.edu>
2015-08-16 22:45:10 -0700

    Added Daily Weather Struct (unfinished)

---
+++ Weather App/DailyWeather.swift
@@ -0,0 +1,43 @@
+//
+//  DailyWeather.swift
+//  Weather App
+//
+//  Created by Brandon Lee on 8/16/15.
+//  Copyright (c) 2015 Brandon Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DailyWeather {
+    
+    let maxTemperature: Int?
+    let minTemperature: Int?
+    let humidity: Int?
+    let precipChance: Int?
+    let summary: String?
+    var icon: UIImage? = UIImage(named: "default.png")
+    var largeIcon: UIImage? = UIImage(named: "default_large.png")
+    let sunriseTime: String?
+    let sunsetTime: String?
+    let day: String?
+
+    init(dailyWeatherDict: [String: AnyObject]) {
+        
+        // Using the optional cast operator (as?) in case the dictionary we pass in doesn't contain the key, will still be stored as an optional
+        maxTemperature = dailyWeatherDict["temperatureMax"] as? Int
+        
+        // Safely cast as Int
+        if let humidityFloat = dailyWeatherDict["humidity"] as? Double {
+            humidity = Int(humidityFloat * 100)
+        } else {
+            humidity = nil
+        }
+        
+        if let precipChanceFloat = dailyWeatherDict["precipProbability"] as? Double {
+            precipChance = Int(precipChanceFloat * 100)
+        } else {
+            precipChance = nil
+        }
+    }
+}
